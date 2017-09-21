130bd2397f671c10d0d7dfadf2e8b017ef79b5d8
danielDaleman <2108419@linux19.labinfo.is.escuelaing.edu.co>
2017-09-21 12:10:18 -0500

    Test registroConsultas

--- src/test/java/tests/ServiciosPacientesTest.java
+++ src/test/java/tests/ServiciosPacientesTest.java
@@ -5,6 +5,9 @@
  */
 package tests;
 
+import edu.eci.pdsw.samples.entities.Consulta;
+import edu.eci.pdsw.samples.services.ExcepcionServiciosPacientes;
+import edu.eci.pdsw.samples.services.impl.ServiciosPacientesMock;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -12,6 +15,16 @@ import static org.junit.Assert.*;
 /**
  *
  * @author hcadavid
+ * 
+ * CLASES DE EQUIVALENCIA
+ *      CE1: Paciente no registrado
+ *           Respuesta esperada: Una Excepcion
+ *      
+ *      CE2: La fecha final sea mayor a la fecha incial
+ *           Respuesta esperada: True 
+ *      
+ *      CE3: Que el paciente  ya existe
+ *           Respuesta esperada: Una Excepcion  
  */
 public class ServiciosPacientesTest {
     
@@ -20,6 +33,18 @@ public class ServiciosPacientesTest {
     
     @Before
     public void setUp() {
+    }
+    
+    @Test 
+    public void testPacienteNoRegistrado() throws ExcepcionServiciosPacientes{
+        ServiciosPacientesMock sm = new ServiciosPacientesMock();
+        Consulta c = new Consulta(java.sql.Date.valueOf("2000-01-01"), "Dolor de estomago", 454);
+        try{
+            sm.agregarConsultaPaciente(123, "cc", c);
+        }catch(ExcepcionServiciosPacientes e){
+            assertEquals(e.getMessage(), "Paciente 123 no esta registrado");
+        }                        
+        
     }
     
 }
