5474bcad658797d84a03667118cc05f1935b5caf
Daniel <dolardanies@hotmail.com>
2017-09-27 09:34:46 -0500

    InicioVistaRegistraConsultas
    Modificacion de registro consulta bean e inicio en xhtml.

--- src/main/java/edu/eci/pdsw/samples/managebeans/RegistroConsultaBean.java
+++ src/main/java/edu/eci/pdsw/samples/managebeans/RegistroConsultaBean.java
@@ -6,10 +6,14 @@
 package edu.eci.pdsw.samples.managebeans;
 
 
+import edu.eci.pdsw.samples.entities.Consulta;
 import edu.eci.pdsw.samples.services.ServiciosHistorialPacientesFactory;
 import edu.eci.pdsw.samples.services.ServiciosPacientes;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import javax.faces.application.FacesMessage;
 
@@ -27,17 +31,75 @@ import javax.faces.bean.SessionScoped;
 public class RegistroConsultaBean implements Serializable {
 
     private final ServiciosPacientes servicepacientes = ServiciosHistorialPacientesFactory.getInstance().getServiciosPaciente();
-
-    
+    private Consulta cons;
+    Set<Consulta> consultas=new LinkedHashSet<>();;
+    private int identificador;
+    private Date fecha;
+    private String descripcion;
+    private long costo;
 
     public RegistroConsultaBean() {
     }
+    
+    public Consulta getCon() {
+        return cons;
+    }
 
+    public void setCon(Consulta con) {
+        this.cons = con;
+    }
+
+    public Consulta getConsulta() {
+        return cons;
+    }
+
+    public void setConsulta(Consulta consulta) {
+        this.cons = consulta;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public long getCosto() {
+        return costo;
+    }
+
+    public void setCosto(long costo) {
+        this.costo = costo;
+    }
+    
     public void showMessage(String estado, String mensaje) {
         FacesMessage message;
         message = new FacesMessage(FacesMessage.SEVERITY_INFO, estado, mensaje);
         RequestContext.getCurrentInstance().showMessageInDialog(message);
     }
+
+    public ServiciosPacientes getServicepacientes() {
+        return servicepacientes;
+    }
+    
+    
 
 
 }
